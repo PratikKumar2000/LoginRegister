64d86e1e2fce6e15baf55d5174e47523e37d5fba
Pratik Kumar <kumarpratik2211@gmail.com>
2023-03-31 22:09:27 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+
+// database connection
+connection();
+
+// middlewares
+app.use(express.json());
+app.use(cors());
+
+// routes
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+
+//static files
+app.use("/", express.static(path.join(__dirname, "Client", "build")));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "Client", "build", "index.html"));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`Listening on port ${port}...`));
