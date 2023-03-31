ae404bfcae19716504eff56846c1229747dc61b9
Pratik Kumar <kumarpratik2211@gmail.com>
2023-03-31 22:18:13 +0530

    made minor change

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 const connection = require("./db");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
